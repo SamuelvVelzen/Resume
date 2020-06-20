92acf87105fb76a731dad40737721f99e2ee76f6
Samuel <samuelvvelzen@gmail.com>
2020-06-20 16:40:17 +0200

    adding bold

--- src/components/shared/navbar.js
+++ src/components/shared/navbar.js
@@ -6,7 +6,7 @@ import ThemeButton from './buttons';
 export default function Navbar() {
     return (
         <div id="navbar">
-            <p className="description">Samuel.</p>
+            <p className="description bold">Samuel.</p>
             <button className="link">Introduction</button>
             <button className="link">Projects</button>
             <ThemeButton />
