106ff6676d414f276f04c9cc4d030ff71afc4c8c
Samuel <samuelvvelzen@gmail.com>
2020-06-20 20:01:54 +0200

    Adding USPS to app

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from './shared/navbar';
 import Hero from './pages/hero';
+import USPS from './pages/usps';
 
 export default function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <USPS />
         </>
     );
 }
