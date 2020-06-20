56436d453e09a56444c7f7ec1eb263e9001c7821
Samuel <samuelvvelzen@gmail.com>
2020-06-20 16:38:15 +0200

    adding navbar

---
+++ src/components/shared/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './../../styles/style/components/shared/navbar.scss';
+import ThemeButton from './buttons';
+
+export default function Navbar() {
+    return (
+        <div id="navbar">
+            <p className="description">Samuel.</p>
+            <button className="link">Introduction</button>
+            <button className="link">Projects</button>
+            <ThemeButton />
+        </div>
+    );
+}
