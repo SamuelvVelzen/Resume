7620bd2c140bfabec87c2a6d15cb1af41c093ccb
Samuel <samuelvvelzen@gmail.com>
2020-06-28 13:17:21 +0200

    projects, colors and required key are now in a config file

---
+++ src/config/projects.js
@@ -0,0 +1,33 @@
+import Vicrea from './../styles/images/content/hero.png';
+
+export const projects = {
+        Vicrea: {
+            image: Vicrea,
+            textColor: 'white',
+            tags: ['HTML', 'CSS', 'JavaScript', 'Gulp'],
+            description: 'lorem test lalalal allalals sdks a hoie soa;',
+        },
+        Indivirtual: {
+            image: Vicrea,
+            textColor: 'white',
+            tags: ['C#', 'HTML', 'SCSS', 'ReactJS'],
+            description:
+                'lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa;',
+        },
+        Test: {
+            image: Vicrea,
+            textColor: 'white',
+            tags: ['C#', 'HTML', 'SCSS'],
+            description: 'lorem test lalalal allalals sdks a hoie soa;',
+        },
+    },
+    requiredKeys = ['image', 'tags', 'textColor', 'description'],
+    allowedColors = [
+        '#441FD9',
+        '#D91F62',
+        '#D91F62',
+        '#D91F62',
+        '#D91F62',
+        '#D91F62',
+        '#D91F62',
+    ];
