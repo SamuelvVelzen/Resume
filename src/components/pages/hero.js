9a21cbe9939111d32ae94ccb250f538c32226b2d
Samuel <samuelvvelzen@gmail.com>
2020-06-28 16:51:58 +0200

    added a list of things i can do

--- src/components/pages/hero.js
+++ src/components/pages/hero.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeContext } from '../../logics/theme-context';
 
 import './../../styles/style/components/pages/hero.scss';
 import HeroImage from './../../styles/images/content/hero.png';
 
 export default function Hero() {
+    const [index, setIndex] = useState(0),
+        workArr = [
+            'code',
+            'JavaScript',
+            'HTML5',
+            'design',
+            'CSS / SCSS',
+            'ReactJS',
+            'Scrum',
+            'Vue',
+            'PHP',
+            'Gulp',
+            'C#',
+        ];
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (index === workArr.length - 1) {
+                setIndex(0);
+                return;
+            }
+            setIndex(index + 1);
+        }, 2000);
+    }, [index, workArr.length]);
+
     return (
         <ThemeContext.Consumer>
             {({ theme, toggleTheme }) => (
@@ -35,7 +60,7 @@ export default function Hero() {
                                     theme.style === 'dark' ? theme.white : null,
                             }}
                         >
-                            and I do <span className="subtitle">game</span>
+                            and I do {workArr[index]}
                         </p>
                     </section>
                     <img
