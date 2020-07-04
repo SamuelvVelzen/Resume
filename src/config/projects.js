adf6fa43f5ccfe4cc6b7c45544582fb51cf69044
Samuel <samuelvvelzen@gmail.com>
2020-07-04 23:37:11 +0200

    improve project page

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,33 +1,33 @@
 import Vicrea from './../styles/images/content/hero.png';
 
 export const projects = {
-        Vicrea: {
-            image: Vicrea,
-            textColor: 'white',
-            tags: ['Gulp'],
-            description: 'lorem test lalalal allalals sdks a hoie soa;',
-        },
-        Indivirtual: {
-            image: Vicrea,
-            textColor: 'white',
-            tags: ['HTML', 'SCSS', 'ReactJS'],
-            description:
-                'lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa;',
-        },
-        Test: {
-            image: Vicrea,
-            textColor: 'white',
-            tags: ['C#', 'HTML', 'SCSS', 'ReactJS'],
-            description: 'lorem test lalalal allalals sdks a hoie soa;',
-        },
+    Vicrea: {
+        image: Vicrea,
+        textColor: 'white',
+        tags: ['Gulp'],
+        description: 'lorem test lalalal allalals sdks a hoie soa;',
     },
-    requiredKeys = ['image', 'tags', 'textColor', 'description'],
-    allowedColors = [
-        '#441FD9',
-        '#D91F62',
-        '#D91F62',
-        '#D91F62',
-        '#D91F62',
-        '#D91F62',
-        '#D91F62',
-    ];
+    Indivirtual: {
+        image: Vicrea,
+        textColor: 'white',
+        tags: ['HTML', 'SCSS', 'ReactJS'],
+        description:
+            'lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa;',
+    },
+    Test: {
+        image: Vicrea,
+        textColor: 'white',
+        tags: ['C#', 'HTML', 'SCSS', 'ReactJS'],
+        description: 'lorem test lalalal allalals sdks a hoie soa;',
+    },
+};
+export const requiredKeys = ['image', 'tags', 'textColor', 'description'];
+export const allowedColors = [
+    '#441fd9',
+    '#d91f62',
+    '#A38560',
+    '#596F62',
+    '#00C49A',
+    '#6bab90',
+    '#41292c',
+];
