9aa2f81d83122d212a06a117738f8f8e13d6906b
Samuel <samuelvvelzen@gmail.com>
2020-07-09 22:16:22 +0200

    added fields in the object to fill the new reference in project file

--- src/config/projects.js
+++ src/config/projects.js
@@ -3,22 +3,60 @@ import Vicrea from './../styles/images/content/hero.png';
 export const projects = {
     Vicrea: {
         image: Vicrea,
+        assignment: 'Buiten Raadplegen',
         textColor: 'white',
         tags: ['Gulp'],
         description: 'lorem test lalalal allalals sdks a hoie soa;',
+        content: {
+            title: 'Vicrea',
+            left: {
+                title: 'hoi',
+                text:
+                    'Excepteur esse magna fugiat cupidatat aliqua cillum deserunt non mollit do. Fugiat anim fugiat ut ipsum. Aliqua duis enim ad irure velit amet anim non veniam. Irure ad elit eu velit sint fugiat cillum eu laborum sunt elit qui ipsum nulla. Nostrud proident cupidatat eu magna sint officia officia velit. In cillum anim consequat amet esse mollit quis consequat in. Occaecat ex esse sint aliquip enim aliqua minim ad voluptate ex adipisicing aute id voluptate. Nostrud pariatur sit ipsum excepteur minim consectetur. Aute Lorem Lorem aliqua non nulla ut excepteur ea. Quis non labore sit commodo quis ex duis enim magna sit exercitation laboris aliquip dolor.',
+            },
+            right: {
+                title: 'neee',
+                text:
+                    'Excepteur esse magna fugiat cupidatat aliqua cillum deserunt non mollit do. Fugiat anim fugiat ut ipsum. Aliqua duis enim ad irure velit amet anim non veniam. Irure ad elit eu velit sint fugiat cillum eu laborum sunt elit qui ipsum nulla. Nostrud proident cupidatat eu magna sint officia officia velit. In cillum anim consequat amet esse mollit quis consequat in. Occaecat ex esse sint aliquip enim aliqua minim ad voluptate ex adipisicing aute id voluptate. Nostrud pariatur sit ipsum excepteur minim consectetur. Aute Lorem Lorem aliqua non nulla ut excepteur ea. Quis non labore sit commodo quis ex duis enim magna sit exercitation laboris aliquip dolor.',
+            },
+        },
     },
     Indivirtual: {
         image: Vicrea,
+        assignment: 'Het Perfecte Plaatje',
         textColor: 'white',
         tags: ['HTML', 'SCSS', 'ReactJS'],
         description:
             'lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa; lorem test lalalal allalals sdks a hoie soa;',
+        content: {
+            title: '',
+            left: {
+                title: '',
+                text: '',
+            },
+            right: {
+                title: '',
+                text: '',
+            },
+        },
     },
-    Test: {
+    Smartphonehoesje: {
         image: Vicrea,
+        assignment: 'De verbeterde webshop',
         textColor: 'white',
         tags: ['C#', 'HTML', 'SCSS', 'ReactJS'],
         description: 'lorem test lalalal allalals sdks a hoie soa;',
+        content: {
+            title: '',
+            left: {
+                title: '',
+                text: '',
+            },
+            right: {
+                title: '',
+                text: '',
+            },
+        },
     },
 };
 export const requiredKeys = ['image', 'tags', 'textColor', 'description'];
