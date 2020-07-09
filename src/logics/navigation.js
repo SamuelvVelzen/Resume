f23aacfae44de376d2c67227b5bb83865866e054
Samuel <samuelvvelzen@gmail.com>
2020-07-10 00:14:56 +0200

    added smoothscroll and polyfill for browsers like safari

--- src/logics/navigation.js
+++ src/logics/navigation.js
@@ -1,1 +1,16 @@
-export const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+import smoothscroll from 'smoothscroll-polyfill';
+
+smoothscroll.polyfill();
+
+export const scrollToRef = (ref) => {
+    const paddingTop = parseFloat(
+        window.getComputedStyle(ref.current).getPropertyValue('padding-top')
+    );
+    console.log(paddingTop);
+
+    window.scrollTo({
+        left: 0,
+        top: ref.current.offsetTop - paddingTop,
+        behavior: 'smooth',
+    });
+};
