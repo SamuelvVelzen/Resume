623085dfdb8cd8ff4f163be9dd70f59a8376503b
Samuel <samuelvvelzen@gmail.com>
2020-07-09 22:14:14 +0200

    improved project with links to the correct object reference

--- src/components/pages/project/project.js
+++ src/components/pages/project/project.js
@@ -3,13 +3,45 @@ import { useLocation } from 'react-router-dom';
 
 import { projects } from './../../../config/projects';
 
+import Navbar from './../../shared/navbar';
+import BigImage from './components/bigimage';
+import SmallImage from './components/smallimage';
+import Summary from './components/summary';
+import TextContent from './components/textcontent';
+import Banner from './components/banner';
+
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
 export default function Project() {
-    let query = useQuery();
+    const query = useQuery(),
+        company = query.get('name'),
+        project = projects[company];
 
-    console.log(projects[query.get('name')]);
+    return (
+        <>
+            <Navbar />
+            <Banner
+                banner={project.image}
+                title={project.assignment}
+                company={company}
+                tags={project.tags}
+            />
+            <Summary title={project.assignment} desc={project.description} />
 
-    return <div>{query.get('name')}</div>;
+            <TextContent
+                title={project.content.title}
+                left={{
+                    title: project.content.left.title,
+                    content: project.content.left.text,
+                }}
+                right={{
+                    title: project.content.right.title,
+                    content: project.content.right.text,
+                }}
+            />
+            <BigImage alt={company} img={project.image} />
+            <SmallImage alt={company} img={project.image} />
+        </>
+    );
 }
