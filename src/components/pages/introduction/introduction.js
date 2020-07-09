2f5c0496092ac03fdecc54621759a9278692f699
Samuel <samuelvvelzen@gmail.com>
2020-07-09 23:57:18 +0200

    added reference to different components in order to scroll to them

--- src/components/pages/introduction/introduction.js
+++ src/components/pages/introduction/introduction.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import Navbar from './../../shared/navbar';
 import Hero from './components/hero';
-import USPS from './components/usps';
-import Project from './components/projects';
+import { USPS } from './components/usps';
+import { Project } from './components/projects';
+
+import { scrollToRef } from '../../../logics/navigation';
 
 export default function App() {
+    const introduction = useRef(null),
+        projects = useRef(null);
+
     return (
         <>
-            <Navbar />
+            <Navbar
+                links={{
+                    Introduction: {
+                        ref: introduction,
+                    },
+                    Projects: {
+                        ref: projects,
+                    },
+                }}
+                action={scrollToRef}
+            />
             <Hero />
-            <USPS />
-            <Project />
+            <USPS ref={introduction} />
+            <Project ref={projects} />
         </>
     );
 }
